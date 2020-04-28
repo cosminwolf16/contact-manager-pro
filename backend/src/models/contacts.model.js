c93e2b8db6267d95dfcbce3cc0a37af16946eb12
Smokyn <smokyn_cosmin@yahoo.com>
2020-04-28 21:36:53 +0300

    setting the backend up and  the contacts model

---
+++ backend/src/models/contacts.model.js
@@ -0,0 +1,37 @@
+require('mongoose-type-email');
+
+module.exports = function (app) {
+  const modelName = 'contacts';
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const schema = new Schema({
+    name : {
+      first: {
+        type: String,
+        required: [true, 'First Name is required']
+      },
+      last: {
+        type: String,
+        required: false
+      }
+    },
+    email : {
+      type: mongooseClient.SchemaTypes.Email,
+      required: [true, 'Email is required']
+    },
+    phone : {
+      type: String,
+      required: [true, 'Phone is required'],
+      validate: {
+        validator: function(v) {
+          return /^\+(?:[0-9] ?){6,14}[0-9]$/.test(v);
+        },
+        message: '{VALUE} is not a valid international phone number!'
+      }
+    }
+  }, {
+    timestamps: true
+  });
+
+  return  mongooseClient.model(modelName,  schema);
+};
