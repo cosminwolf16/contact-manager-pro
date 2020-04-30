976da601895c55d54b5937d8484ee4c7abaa3d18
Smokyn <smokyn_cosmin@yahoo.com>
2020-04-30 18:37:49 +0300

    contact list and contact card compont created

--- client/src/components/contact-list.js
+++ client/src/components/contact-list.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react';
+import ContactCard from './contact-card';
 
-const ContactList = () => {
-  return (
-    <div>
-      <p>No contacts here</p>
-    </div>
-  );
-};
+export default function ContactList({ contacts }) {
+  const cards = () => {
+    return contacts.map((contact) => {
+      return <ContactCard key={contact._id} contact={contact}></ContactCard>;
+    });
+  };
 
-export default ContactList;
+  return <Card.Group>{cards()}</Card.Group>;
+}
--- client/src/pages/contact-list-page.js
+++ client/src/pages/contact-list-page.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import ContactList from '../components/contact-list';
+import { ContactContext } from '../context/contact-context';
 
-const ContactListPage = () => {
+const data = [
+  {
+    _id: '1',
+    name: {
+      first: 'John',
+      last: 'Doe',
+    },
+    phone: '555',
+    email: 'john@gmail.com',
+  },
+  {
+    _id: '2',
+    name: {
+      first: 'Bruce',
+      last: 'Wayne',
+    },
+    phone: '777',
+    email: 'bruce.wayne@gmail.com',
+  },
+];
+
+export default function ContactListPage() {
+  const [state, dispatch] = useContext(ContactContext);
+
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_CONTACTS',
+      payload: data,
+    });
+  }, [dispatch]);
+
   return (
     <div>
       <h1>List of Contacts</h1>
-      <ContactList />
+      <ContactList contacts={state.contacts} />
     </div>
   );
-};
-
-export default ContactListPage;
+}
