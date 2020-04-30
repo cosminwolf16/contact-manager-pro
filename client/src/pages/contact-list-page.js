3aa32c1ec7cc86da2e82ec31e6f22eed02c85470
Smokyn <smokyn_cosmin@yahoo.com>
2020-04-30 19:11:36 +0300

    error messages fully received

--- client/src/pages/contact-list-page.js
+++ client/src/pages/contact-list-page.js
@@ -2,17 +2,22 @@ import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
 import ContactList from '../components/contact-list';
 import { ContactContext } from '../context/contact-context';
+import FlashMessage, { flashErrorMessage } from '../components/flash-message';
 
 export default function ContactListPage() {
   const [state, dispatch] = useContext(ContactContext);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get('http://localhost:3030/contacts');
-      dispatch({
-        type: 'FETCH_CONTACTS',
-        payload: response.data.data || response.data,
-      });
+      try {
+        const response = await axios.get('http://localhost:3030/contacts');
+        dispatch({
+          type: 'FETCH_CONTACTS',
+          payload: response.data.data || response.data,
+        });
+      } catch (error) {
+        flashErrorMessage(dispatch, error);
+      }
     };
     fetchData();
   }, [dispatch]);
@@ -20,6 +25,7 @@ export default function ContactListPage() {
   return (
     <div>
       <h1>List of Contacts</h1>
+      {state.message.content && <FlashMessage message={state.message} />}
       <ContactList contacts={state.contacts} />
     </div>
   );
